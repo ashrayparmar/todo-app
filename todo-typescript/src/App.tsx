e31a1e56a00334a14b6a0a47877bf71e5d6e4624
pashray <ashrayparmar0307@gmail.com>
2022-05-06 19:42:32 +0530

    feat: listing todos in the UI

--- todo-typescript/src/App.tsx
+++ todo-typescript/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
 import { Todo } from "./model";
 
 const App: React.FC = () => {
@@ -12,7 +13,7 @@ const App: React.FC = () => {
     e.preventDefault();
     if (todo) {
       setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
-      setTodo("")
+      setTodo("");
     }
   };
 
@@ -22,6 +23,7 @@ const App: React.FC = () => {
     <div className="App">
       <span className="heading">Todo list</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 };
