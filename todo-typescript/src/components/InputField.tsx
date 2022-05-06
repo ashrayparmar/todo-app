5074cbe92b7842e74d03fc0b036ba5b82d1e0d2b
pashray <ashrayparmar0307@gmail.com>
2022-05-06 18:53:43 +0530

    feat: todo submit button added

--- todo-typescript/src/components/InputField.tsx
+++ todo-typescript/src/components/InputField.tsx
@@ -4,15 +4,18 @@ import "./style.css";
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
   return (
-    <form className="input">
+    <form className="input" onSubmit={handleAdd}>
       <input
         type="input"
         placeholder="Enter a task"
         className="input_box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
       ></input>
       <button type="submit" className="input_submit">
         Go
